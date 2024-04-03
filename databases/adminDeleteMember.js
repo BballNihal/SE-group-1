d96deeb6a793ef1966c6803f13520dd6cef60867
chunkymagic <164483470+chunkymagic@users.noreply.github.com>
2024-04-02 21:20:54 -0400

    Update adminDeleteMember.js

--- databases/adminDeleteMember.js
+++ databases/adminDeleteMember.js
@@ -4,15 +4,28 @@ const sqlite3 = require('sqlite3').verbose();
 const stringHash = require('./passwordHash.js');
 
 function adminDeleteMember(req, res, member, memberdb) {
-    //const member = JSON.parse(body);
+  
+    if (!member.hasOwnProperty('memberID')) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end(`Missing required property: memberID`);
+        return;
+    }
+
     const memberID = member.memberID;
 
-    memberdb.run(`DELETE FROM members WHERE memberID = ?`, memberID, function (err) {
+    const memberIDRegex = /^M\d{10}$/;
+    if (!memberIDRegex.test(member.memberID)) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid MemberID');
+        return;
+    }
+
+    memberdb.run(`DELETE FROM members WHERE memberID = ?`, member.memberID, function (err) {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Database error');
         } else {
-            res.end(`Member deleted successfully. Your member ID was ${memberID}`);
+            res.end(`Member deleted successfully. Your member ID was ${member.memberID}`);
         }
     });
 }
