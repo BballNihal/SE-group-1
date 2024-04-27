0663b768cdc8b3945a4bbb5c6c14a0a33233788e
chunkymagic <164483470+chunkymagic@users.noreply.github.com>
2024-04-27 19:51:23 -0400

    Add files via upload

---
+++ databases/adminGetMember.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+const sqlite3 = require('sqlite3').verbose();
+const stringHash = require('./passwordHash.js');
+
+function adminGetMember(req, res, member, memberdb) {
+    const memberID = member.memberID;
+
+    memberdb.get(`SELECT memberID, username, phoneNumber, adminStatus FROM members WHERE memberID = ?`, [memberID], function(err, row) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(`Database error: ${err}`);
+        } else if (row) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(row));
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end(`Member not found`);
+        }
+    });
+}
+
+module.exports = adminGetMember;
