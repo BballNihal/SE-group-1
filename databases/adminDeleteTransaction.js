fa42cf3f61fb010330aae0d2ce5e681ab84f1bcc
ttv13 <165097755+ttv13@users.noreply.github.com>
2024-04-29 23:46:15 -0400

    Update adminDeleteTransaction.js
    CHango CBigo

--- databases/adminDeleteTransaction.js
+++ databases/adminDeleteTransaction.js
@@ -1,21 +1,56 @@
+
 const http = require('http');
 const url = require('url');
 const sqlite3 = require('sqlite3').verbose();
+const dataValidation = require('./dataValidation.js');
+const adminGetTransaction = require('./adminGetTransaction.js');
 
-function adminDeleteTransaction(res,requestData,transactiondb){
+async function adminDeleteTransaction(res, requestData, transactiondb) {
 
-    transactiondb.run(`DELETE FROM transactions WHERE orderId = ?`, requestData.orderId, function (err) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end(`Transaction Database error: ${err} `);
-            return;
-        } else {
-            res.end(`Transaction ${requestData.orderId} sucessfully deleted `);
+    //--data validation --
+    let requiredProperties = ['orderId'];
 
-        }
-    }); //end of database delete
+    if (!(dataValidation(res, requestData, requiredProperties))) {
+        return;
+    }
+    let writeGet = 0;
+
+    try {
+
+        var row = await adminGetTransaction(res, requestData, transactiondb, writeGet);
+
+        transactiondb.run(`DELETE FROM transactions WHERE orderId = ?`, requestData.orderId, function (err) {
+            if (err) {
+
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end(`Transaction Database error: ${err} `);
+                return;
+
+            } else if (this.changes === 0) {
+
+                res.writeHead(400, { 'Content-Type': 'text/plain' });
+                res.end(`Error : No matching orderId in Database :${requestData.orderId}`);
+                return;
+
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(`Transaction ${requestData.orderId} sucessfully deleted for productid ${row.productId}`);
+
+            }
+        }); //end of database delete
+
+
+    } catch (err) {
+
+        console.log(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`Error : Deleting Transaction ${err}`);
+
+
+    }// End of try catch
+
 
     return;
-}
+}//End of function
 
 module.exports = adminDeleteTransaction;
