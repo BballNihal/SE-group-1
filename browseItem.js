8bc20ac10f733483482ec590f7a72ed043fc4a4c
leoZqf <leozqf@gmail.com>
2024-04-30 06:48:25 -0400

    browse search Liteversion function

--- browseItem.js
+++ browseItem.js
@@ -74,4 +74,68 @@ function browseItem(request, response) {
     });
 }
 
-module.exports = browseItem;
+function browseItemLite(request, response) {
+    const dBCon = connectToLiteDatabase(); 
+
+    // Extract productID from the request query parameters
+    const parsedUrl = url.parse(request.url, true);
+    const productID = parsedUrl.query.productID;
+
+    // Validate product ID
+    if (!isValidProductID(productID)) {
+        const resMsg = {
+            code: 400,
+            body: "Invalid product ID format"
+        };
+        response.writeHead(resMsg.code, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(resMsg.body));
+        return;
+    }
+
+    // Construct SQL statement to search products table based on product ID
+    const sqlStatement = `
+        SELECT nameVar, descriptionVar, manufacturer, price FROM products WHERE productID = ?;
+    `;
+
+    // Execute the SQL query
+    dBCon.all(sqlStatement, [productID], (error, results) => {
+        if (error) {
+            console.error("Error executing SQL query:", error);
+            const resMsg = {
+                code: 500,
+                body: "Internal Server Error"
+            };
+            response.writeHead(resMsg.code, { "Content-Type": "application/json" });
+            response.end(JSON.stringify(resMsg.body));
+        } else {
+            if (results.length > 0) {
+                const productDetails = {
+                    name: results[0].nameVar,
+                    description: results[0].descriptionVar,
+                    manufacturer: results[0].manufacturer,
+                    price: results[0].price
+                };
+                const resMsg = {
+                    code: 200,
+                    body: productDetails
+                };
+                response.writeHead(resMsg.code, { "Content-Type": "application/json" });
+                response.end(JSON.stringify(resMsg.body));
+            } else {
+                const resMsg = {
+                    code: 404,
+                    body: "Product not found"
+                };
+                response.writeHead(resMsg.code, { "Content-Type": "application/json" });
+                response.end(JSON.stringify(resMsg.body));
+            }
+        }
+
+        // Close the database connection after sending the response
+        dBCon.close();
+
+        return resMsg.body;
+    });
+}
+
+module.exports = browseItem, browseItemLite;
